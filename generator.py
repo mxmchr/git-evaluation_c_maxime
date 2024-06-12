19f5369f50255929efbc52f9b56ef94657ab4095
mxmchr <chretien.maxime72@gmail.com>
2024-06-12 20:37:34 +0200

    Add functionality to generate specified number of expressions

--- generator.py
+++ generator.py
@@ -1,12 +1,23 @@
 import random
+import sys
 
 def generate_expression():
     num1 = random.randint(1, 1000)
     num2 = random.randint(1, 1000)
     operator = random.choice(['+', '-', '*', '/'])
-    expression = f"{num1}{operator}{num2}"
+    expression = f"{num1} {operator} {num2}"
     return expression
 
-expression = generate_expression()
+if len(sys.argv) != 2:
+    print("Usage: python3 generator.py <nombre d'expressions voulu>")
+    sys.exit(1)
 
-print(expression)
+try:
+    num_expressions = int(sys.argv[1])
+except ValueError:
+    print("Le nombre d'expressions doit être un entier.")
+    sys.exit(1)
+
+for _ in range(num_expressions):
+    expression = generate_expression()
+    print(expression)
